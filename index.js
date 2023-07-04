d42518654772bc41b174f6b3f8a966be711de92b
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-07-04 06:01:21 +0100

    working with javascript scripting and code syntax

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // VARIABLES
 // Using meaningful and pronounceable variable names
+
+const { isEmptyBindingElement } = require("typescript");
+
 // Bad
 const yyyymmdstr = moment().format("YYYY/MM/DD");
 // Good
@@ -325,3 +328,165 @@ function createMenu(config) {
 
 createMenu(menuConfig);
 
+// Don't use flags as function parameters
+// Bad
+function createFile(name, temp) {
+    if (temp) {
+        fs.create(`./temp/${name}`);
+    } else {
+        fs.create(name);
+    }
+}
+
+// Good
+function createFile(name) {
+    fs.create(name);
+}
+
+function createTempFile(name) {
+    createFile(`./temp/${name}`);
+}
+
+
+// Avoid Side Effects (part 1)
+// Bad
+// Global variable referenced by following function.
+// If we had another function that used this name, now it'd be an array and it could break it.
+let name = "Ryan McDermott";
+
+function splitIntoFirstAndLastName() {
+    name = name.split(" ");
+}
+splitIntoFirstAndLastName();
+console.log(name); // ['Ryan', 'McDermott'];
+
+// Good
+function splitIntoFirstAndLastName(name) {
+    return name.split(" ");
+}
+const name = "Ryan McDermott";
+const newName = splitIntoFirstAndLastName(name);
+
+console.log(name); // 'Ryan McDermott';
+console.log(newName); // ['Ryan', 'McDermott'];
+
+
+// Avoid Side Effects (part 2)
+// Bad
+const addItemToCart = (cart, item) => {
+    cart.push({ item, date: Date.now() });
+};
+
+// Good
+const addItemToCart = (cart, item) => {
+    return [...cart, { item, date: Date.now() }];
+}
+
+
+// Don't write to global functions
+// Bad
+Array.prototype.diff = function diff(comparisonArray) {
+    const hash = new Set(comparisonArray);
+    return this.filter(elem => !hash.has(elem));
+};
+
+// Good
+class SuperArray extends Array {
+    diff(comparisonArray) {
+        const hash = new Set(comparisonArray);
+        return this.filter(elem => !hash.has(elem));
+    }
+}
+
+
+// Favor functional programming over imperative programming
+// Bad
+const programmerOutput = [
+    {
+        name: "Uncle Bobby",
+        linesOfCode: 500
+    },
+    {
+        name: "Suzie Q",
+        linesOfCode: 1500
+    },
+    {
+        name: "Jimmy Gosling",
+        linesOfCode: 150
+    },
+    {
+        name: "Gracie Hopper",
+        linesOfCode: 1000
+    },
+];
+
+let totalOutput = 0;
+
+for (let i = 0; i < programmerOutput.length; i++) {
+    totalOutput += programmerOutput[i].linesOfCode;
+}
+
+// Good
+const programmerOutput = [
+    {
+        name: "Uncle Bobby",
+        linesOfCode: 500,
+    },
+    {
+        name: "Suzie Q",
+        linesOfCode: 1500,
+    },
+    {
+        name: "Jimmy Gosling",
+        linesOfCode: 150,
+    },
+    {
+        name: "Gracie Hopper",
+        linesOfCode: 1000,
+    },
+];
+
+const totalOutput = programmerOutput.reduce(
+    (totalLines, output) => totalLines + output.linesOfCode,
+    0
+);
+
+
+// Encapsulate conditionals
+// Bad
+if (fsm.state === "fetching" && isEmptyBindingElement(listNode)) {
+    // ... 
+}
+
+// Good
+function shouldShowSpinner(fsm, listNode) {
+    return fsm.state === "fetching" && isEmpty(listNode);
+}
+
+if (shouldShowSpinner(fsmInstance, listNodeInstance)) {
+    // ... 
+}
+
+
+// Avoid negative conditionals
+// Bad
+function isDOMNodeNotPresent(node) {
+    // ... 
+}
+
+if (!isDOMNodeNotPresent(node)) {
+    // ...
+}
+
+// Good
+function isDOMNodePresent(node) {
+    // ...
+}
+
+if (isDOMNodeNotPresent(node)) {
+    // ... 
+}
+
+
+
+// Avoid conditionals
